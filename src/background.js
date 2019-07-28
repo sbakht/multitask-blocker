f07038c09b40d228ba3fae065717b5e9145aef69
Saad Bakht <rotk13@gmail.com>
2019-07-27 20:05:36 -0400

    getting started on this again - storing tab data on tab switching

--- src/background.js
+++ src/background.js
@@ -1,14 +1,25 @@
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
+const tabHistory = [];
+
+function getTabName(id, fn) {
+  return chrome.tabs.get(id, fn);
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
+  });
+
+  chrome.tabs.onActivated.addListener(function(tab) {
+    //$('body').append('hi');
+    console.log(tab);
+    getTabName(tab.tabId, function(tab) {
+
+      tabHistory.push(tab.title);
+
+      chrome.storage.sync.set({pavTabs: JSON.stringify(tabHistory)}, function() {
+        console.log('The color is green.');
+      });
     });
   });
+
+});
